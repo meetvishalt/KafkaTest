3dc849c40738b65d737aee5d866820fd961b59b9
meetvishalt <31311871+meetvishalt@users.noreply.github.com>
2019-08-12 09:31:41 +0000

    Add files via upload

---
+++ dotnetcore-kafka-integration-master/Api/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ProducerConfig config;
+        public OrderController(ProducerConfig config)
+        {
+            this.config = config;
+
+        }
+        // POST api/values
+        [HttpPost]
+        public async Task<ActionResult> PostAsync([FromBody]OrderRequest value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //Serialize 
+            string serializedOrder = JsonConvert.SerializeObject(value);
+
+            Console.WriteLine("========");
+            Console.WriteLine("Info: OrderController => Post => Recieved a new purchase order:");
+            Console.WriteLine(serializedOrder);
+            Console.WriteLine("=========");
+
+            var producer = new ProducerWrapper(this.config,"orderrequests");
+            await producer.writeMessage(serializedOrder);
+
+            return Created("TransactionId", "Your order is in progress");
+        }
+    }
+}
---
+++ dotnetcore-kafka-integration-master/Api/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using HostedServices = Microsoft.Extensions.Hosting;
+using Api.Services;
+using Confluent.Kafka;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<HostedServices.IHostedService, ProcessOrdersService>();
+            
+            var producerConfig = new ProducerConfig();
+            var consumerConfig = new ConsumerConfig();
+            Configuration.Bind("producer",producerConfig);
+            Configuration.Bind("consumer",consumerConfig);
+
+            services.AddSingleton<ProducerConfig>(producerConfig);
+            services.AddSingleton<ConsumerConfig>(consumerConfig);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
